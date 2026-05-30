50bb603c5c09487478f091d1bd070ff60d47a8bc
sohamtechuk <info@theinnerpeace.org>
2026-05-30 22:24:47 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-basic.show_icon(IconNames.HEART)
-
 def on_forever():
-    for i in range(4):
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 120)
-        basic.pause(1000)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 120)
+    basic.pause(1000)
 
-        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
-        basic.pause(200)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 120)
+    basic.pause(450)
 
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 120)
-        basic.pause(450)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 120)
+    basic.pause(1000)
 
-        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
-        basic.pause(200)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 120)
+    basic.pause(450)
+
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+    basic.pause(500)
 
 basic.forever(on_forever)
